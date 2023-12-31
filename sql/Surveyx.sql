89db91960ee12485eee6f980462b273c24911aab
Illya Katynskyi <katinskyi.illia@lll.kpi.ua>
2023-12-31 16:20:40 +0200

    Change name of project

--- sql/Surveyx.sql
+++ sql/Surveyx.sql
@@ -3,22 +3,22 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema Surveyx
+-- Schema SurveyX
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `Surveyx` ;
+DROP SCHEMA IF EXISTS `SurveyX` ;
 
 -- -----------------------------------------------------
--- Schema Surveyx
+-- Schema SurveyX
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `Surveyx` DEFAULT CHARACTER SET utf8 ;
-USE `Surveyx` ;
+CREATE SCHEMA IF NOT EXISTS `SurveyX` DEFAULT CHARACTER SET utf8 ;
+USE `SurveyX` ;
 
 -- -----------------------------------------------------
--- Table `Surveyx`.`Role`
+-- Table `SurveyX`.`Role`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `Surveyx`.`Role` ;
+DROP TABLE IF EXISTS `SurveyX`.`Role` ;
 
-CREATE TABLE IF NOT EXISTS `Surveyx`.`Role` (
+CREATE TABLE IF NOT EXISTS `SurveyX`.`Role` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(50) NOT NULL,
   `description` VARCHAR(45) NULL,
@@ -27,11 +27,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Surveyx`.`User`
+-- Table `SurveyX`.`User`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `Surveyx`.`User` ;
+DROP TABLE IF EXISTS `SurveyX`.`User` ;
 
-CREATE TABLE IF NOT EXISTS `Surveyx`.`User` (
+CREATE TABLE IF NOT EXISTS `SurveyX`.`User` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `password` VARCHAR(45) NOT NULL,
   `name` VARCHAR(45) NOT NULL,
@@ -44,18 +44,18 @@ CREATE TABLE IF NOT EXISTS `Surveyx`.`User` (
   INDEX `fk_User_Role_idx` (`Role_id` ASC) VISIBLE,
   CONSTRAINT `fk_User_Role`
     FOREIGN KEY (`Role_id`)
-    REFERENCES `Surveyx`.`Role` (`id`)
+    REFERENCES `SurveyX`.`Role` (`id`)
     ON DELETE CASCADE
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Surveyx`.`Quiz`
+-- Table `SurveyX`.`Quiz`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `Surveyx`.`Quiz` ;
+DROP TABLE IF EXISTS `SurveyX`.`Quiz` ;
 
-CREATE TABLE IF NOT EXISTS `Surveyx`.`Quiz` (
+CREATE TABLE IF NOT EXISTS `SurveyX`.`Quiz` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `description` VARCHAR(45) NULL,
   `name` VARCHAR(45) NOT NULL,
@@ -64,11 +64,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Surveyx`.`Question`
+-- Table `SurveyX`.`Question`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `Surveyx`.`Question` ;
+DROP TABLE IF EXISTS `SurveyX`.`Question` ;
 
-CREATE TABLE IF NOT EXISTS `Surveyx`.`Question` (
+CREATE TABLE IF NOT EXISTS `SurveyX`.`Question` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `type` VARCHAR(45) NOT NULL,
   `number` INT UNSIGNED NOT NULL,
@@ -78,18 +78,18 @@ CREATE TABLE IF NOT EXISTS `Surveyx`.`Question` (
   INDEX `fk_Question_Quiz1_idx` (`Quiz_id` ASC) VISIBLE,
   CONSTRAINT `fk_Question_Quiz1`
     FOREIGN KEY (`Quiz_id`)
-    REFERENCES `Surveyx`.`Quiz` (`id`)
+    REFERENCES `SurveyX`.`Quiz` (`id`)
     ON DELETE CASCADE
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Surveyx`.`Answer`
+-- Table `SurveyX`.`Answer`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `Surveyx`.`Answer` ;
+DROP TABLE IF EXISTS `SurveyX`.`Answer` ;
 
-CREATE TABLE IF NOT EXISTS `Surveyx`.`Answer` (
+CREATE TABLE IF NOT EXISTS `SurveyX`.`Answer` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `text` VARCHAR(100) NULL,
   `option` VARCHAR(45) NULL,
@@ -99,18 +99,18 @@ CREATE TABLE IF NOT EXISTS `Surveyx`.`Answer` (
   INDEX `fk_Answer_Question1_idx` (`Question_id` ASC) VISIBLE,
   CONSTRAINT `fk_Answer_Question1`
     FOREIGN KEY (`Question_id`)
-    REFERENCES `Surveyx`.`Question` (`id`)
+    REFERENCES `SurveyX`.`Question` (`id`)
     ON DELETE CASCADE
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Surveyx`.`Respondent`
+-- Table `SurveyX`.`Respondent`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `Surveyx`.`Respondent` ;
+DROP TABLE IF EXISTS `SurveyX`.`Respondent` ;
 
-CREATE TABLE IF NOT EXISTS `Surveyx`.`Respondent` (
+CREATE TABLE IF NOT EXISTS `SurveyX`.`Respondent` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `User_id` INT NOT NULL,
   `Answer_id` INT NOT NULL,
@@ -119,23 +119,23 @@ CREATE TABLE IF NOT EXISTS `Surveyx`.`Respondent` (
   INDEX `fk_Respondent_Answer1_idx` (`Answer_id` ASC) VISIBLE,
   CONSTRAINT `fk_Respondent_User1`
     FOREIGN KEY (`User_id`)
-    REFERENCES `Surveyx`.`User` (`id`)
+    REFERENCES `SurveyX`.`User` (`id`)
     ON DELETE CASCADE
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Respondent_Answer1`
     FOREIGN KEY (`Answer_id`)
-    REFERENCES `Surveyx`.`Answer` (`id`)
+    REFERENCES `SurveyX`.`Answer` (`id`)
     ON DELETE CASCADE
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Surveyx`.`Option`
+-- Table `SurveyX`.`Option`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `Surveyx`.`Option` ;
+DROP TABLE IF EXISTS `SurveyX`.`Option` ;
 
-CREATE TABLE IF NOT EXISTS `Surveyx`.`Option` (
+CREATE TABLE IF NOT EXISTS `SurveyX`.`Option` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `description` VARCHAR(45) NULL,
   `number` INT UNSIGNED NOT NULL,
@@ -144,30 +144,30 @@ CREATE TABLE IF NOT EXISTS `Surveyx`.`Option` (
   INDEX `fk_Option_Question1_idx` (`Question_id` ASC) VISIBLE,
   CONSTRAINT `fk_Option_Question1`
     FOREIGN KEY (`Question_id`)
-    REFERENCES `Surveyx`.`Question` (`id`)
+    REFERENCES `SurveyX`.`Question` (`id`)
     ON DELETE CASCADE
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Surveyx`.`SelectedOption`
+-- Table `SurveyX`.`SelectedOption`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `Surveyx`.`SelectedOption` ;
+DROP TABLE IF EXISTS `SurveyX`.`SelectedOption` ;
 
-CREATE TABLE IF NOT EXISTS `Surveyx`.`SelectedOption` (
+CREATE TABLE IF NOT EXISTS `SurveyX`.`SelectedOption` (
   `Answer_id` INT NOT NULL,
   `Option_id` INT NOT NULL,
   INDEX `fk_SelectedOption_Answer1_idx` (`Answer_id` ASC) VISIBLE,
   INDEX `fk_SelectedOption_Option1_idx` (`Option_id` ASC) VISIBLE,
   CONSTRAINT `fk_SelectedOption_Answer1`
     FOREIGN KEY (`Answer_id`)
-    REFERENCES `Surveyx`.`Answer` (`id`)
+    REFERENCES `SurveyX`.`Answer` (`id`)
     ON DELETE CASCADE
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_SelectedOption_Option1`
     FOREIGN KEY (`Option_id`)
-    REFERENCES `Surveyx`.`Option` (`id`)
+    REFERENCES `SurveyX`.`Option` (`id`)
     ON DELETE CASCADE
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -178,12 +178,12 @@ SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 -- -----------------------------------------------------
--- Data for table `Surveyx`.`Role`
+-- Data for table `SurveyX`.`Role`
 -- -----------------------------------------------------
 START TRANSACTION;
-USE `Surveyx`;
-INSERT INTO `Surveyx`.`Role` (`name`, `description`) VALUES ('Respondent', 'A user that taking a survey');
-INSERT INTO `Surveyx`.`Role` (`name`, `description`) VALUES ('Editor', 'The user who edits the survey');
-INSERT INTO `Surveyx`.`Role` (`name`, `description`) VALUES ('Admin', 'The user is the owner of the survey');
+USE `SurveyX`;
+INSERT INTO `SurveyX`.`Role` (`name`, `description`) VALUES ('Respondent', 'A user that taking a survey');
+INSERT INTO `SurveyX`.`Role` (`name`, `description`) VALUES ('Editor', 'The user who edits the survey');
+INSERT INTO `SurveyX`.`Role` (`name`, `description`) VALUES ('Admin', 'The user is the owner of the survey');
 
 COMMIT;
